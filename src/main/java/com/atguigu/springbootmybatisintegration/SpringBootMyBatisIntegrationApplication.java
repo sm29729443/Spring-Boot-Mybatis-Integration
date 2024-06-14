fc2660da582c63c6cc4985aa8ace3f5a9157bfb2
Fish <sm29729443@gmail.com>
2024-06-14 15:59:44 +0800

    springboot集成mybatis-plus

---
+++ src/main/java/com/atguigu/springbootmybatisintegration/SpringBootMyBatisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springbootmybatisintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyBatisIntegrationApplication.class, args);
+    }
+
+}
